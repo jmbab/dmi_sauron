563649f8e1561f97964e2519764e1ba1243024a9
jean591g <jean591g@stud.kea.dk>
2022-06-08 14:20:10 +0200

    JSON file transfer Client to Server v1

--- src/Client_03.java
+++ src/Client_03.java
@@ -1,2 +1,28 @@
+import java.io.*;
+import java.net.Socket;
+
 public class Client_03 {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        String host = "127.0.0.1";
+
+        socket = new Socket(host, 4444);
+
+        File file = new File("/Users/jean/Documents/GitHub/dmi_sauron/src/example.json");
+        // Get the size of the file
+        long length = file.length();
+        byte[] bytes = new byte[16 * 1024];
+        InputStream in = new FileInputStream(file);
+        OutputStream out = socket.getOutputStream();
+
+        int count;
+        while ((count = in.read(bytes)) > 0) {
+            out.write(bytes, 0, count);
+        }
+
+        out.close();
+        in.close();
+        socket.close();
+    }
 }
